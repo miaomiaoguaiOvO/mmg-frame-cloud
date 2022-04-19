cc1196c4b19d1d264e40e946b2160f722f02286e
Fan <1733535060@qq.com>
2022-04-19 09:28:24 +0800

    swagger调整，去除devtools

--- cloud-user-consumer/src/main/java/com/mmg/user/consumer/config/swagger/SwaggerConfig.java
+++ cloud-user-consumer/src/main/java/com/mmg/user/consumer/config/swagger/SwaggerConfig.java
@@ -1,20 +1,19 @@
 package com.mmg.user.consumer.config.swagger;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -30,26 +29,44 @@ public class SwaggerConfig {
     private String serverName;
 
     @Bean
-    public Docket createRestApi() {
-        //添加head参数配置end
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build()
-                .globalOperationParameters(parameters());
-
+    public Docket webApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .forCodeGeneration(true)
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .select()
+                //匹配生成
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .build()
+                .useDefaultResponseMessages(false)
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
-    private List<Parameter> parameters() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
-        return pars;
+    /**
+     * 设置授权信息
+     */
+    private List<SecurityScheme> securitySchemes() {
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", In.HEADER.toValue());
+        return Collections.singletonList(apiKey);
+    }
+
+    /**
+     * 授权信息全局应用
+     */
+    private List<SecurityContext> securityContexts() {
+        return Collections.singletonList(
+                SecurityContext.builder()
+                        .securityReferences(Collections.singletonList(new SecurityReference("Authorization", new AuthorizationScope[]{new AuthorizationScope("global", "全局token")})))
+                        .build()
+        );
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title(serverName)
                 .description(serverName)
+                .contact(new Contact("renjie", "", "1733535060@qq.com"))
                 .version("1.0")
                 .build();
     }
