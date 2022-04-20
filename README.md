1763f2fb66020aba78314ac12b4cb7c26c4e461f
Fan <1733535060@qq.com>
2022-04-20 13:09:22 +0800

    接口文档提醒

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mmg-frame-cloud
-网关（webflux）集成spring security鉴权
+网关（webflux）集成spring security鉴权\
+启动provider、consumer后启动gateway即可访问接口文档
--- gateway-security/src/main/java/com/mmg/gateway/security/GatewaySecurityApplication.java
+++ gateway-security/src/main/java/com/mmg/gateway/security/GatewaySecurityApplication.java
@@ -1,5 +1,6 @@
 package com.mmg.gateway.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +16,10 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.mmg.gateway.security", "com.mmg.datasource"})
 @MapperScan(basePackages = {"com.mmg.datasource.mapper"})
+@Slf4j
 public class GatewaySecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewaySecurityApplication.class, args);
+        log.info("访问接口文档----->{}", "http://localhost/swagger-ui/index.html");
     }
 }
